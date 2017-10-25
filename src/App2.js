4342636d6d4733fc819bdc2c4d54569191ec28f5
Adam Becvar <adam.becvar.cz@gmail.com>
2017-10-25 23:03:44 +0200

    Add saving, editing and deleting

--- src/App2.js
+++ src/App2.js
@@ -1,29 +1,62 @@
 //Create a new file App2.js, and copy all from this file into is
 
 import React from "react"
-import { BrowserRouter as Router, Route, Link, NavLink, Switch, Prompt } from "react-router-dom";
+import {BrowserRouter as Router, Route, Link, NavLink, Switch, Prompt} from "react-router-dom";
 
 //Todo: This component must be completed
 class AddBook extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { "book": { title: "", info: "" }, isDirty: false }
+        this.state = {book: {title: "", info: ""}, isDirty: false}
     }
-    onSave = () => {}
-    onChange = (e) => {}
+
+    onSave = () => {
+        this.props.bookStore.addBook(this.state.book)
+        this.setState({book: {title: "", info: ""}, isDirty: false})
+        this.props.onBookStoreChange();
+    }
+    onChange = (event) => {
+        const target = event.target;
+        const newBookState = this.state.book;
+        newBookState[target.name] = target.value
+
+        this.setState((prevState) => ({
+            book: newBookState,
+            isDirty: true
+        }));
+    }
+
     render() {
         return (
             <div>
-                Title: <input name="title"  />
-                Info: <input name="info" />
-                <button onClick={this.onSave}>Save</button>
-                {/*
-                 <Prompt
-                 when={this.state.isDirty}
-                 message="Yoy have unsaved data that will be lost!"
-                 />*/}
+                Title: <input name="title" onChange={this.onChange} value={this.state.book.title}/>
+                Info: <input name="info" onChange={this.onChange} value={this.state.book.info}/>
+                <button onClick={this.onSave}>{this.props.buttonText}</button>
+                {<Prompt when={this.state.isDirty} message="Yoy have unsaved data that will be lost!"/>}
             </div>
         )
+    }
+}
+
+class EditBook extends AddBook {
+    constructor(props) {
+        super(props);
+        this.initProps(props)
+    }
+
+    componentWillReceiveProps(newProps) {
+        this.initProps(newProps);
+    }
+
+    initProps(newProps) {
+        const book = newProps.bookStore.getBook(Number(newProps.match.params.id))
+        this.state = {book: book, isDirty: false}
+    }
+
+    onSave = () => {
+        this.props.bookStore.editBook(this.state.book)
+        this.setState({book: {title: "", info: ""}, isDirty: false})
+        this.props.onBookStoreChange();
     }
 }
 
@@ -44,18 +77,24 @@ const Company = () => {
         </div>
     )
 }
+
 class Product extends React.Component {
     constructor(props) {
         super(props);
-        console.log("props",props);
-        this.state = { bookStore: props.bookStore }
+        console.log("props", props);
+        this.state = {bookStore: props.bookStore}
     }
 
-    onBookWasAdded = ()=>{
+    onBookstoreWasChanged = () => {
         //Nice and easy way to force a rerender
         this.forceUpdate();
     }
 
+    handleDeleteBook = (event, id) => {
+        event.preventDefault()
+        this.props.bookStore.deleteBook(id)
+        this.forceUpdate();
+    }
 
     render() {
         const books = this.state.bookStore.books;
@@ -65,35 +104,42 @@ class Product extends React.Component {
             <h2>Our Products</h2>
             <h4>All our great books </h4>
             <ul>
-                {books.map((book) => <li key={book.id}>
-                    <NavLink activeClassName="activeV2"
-                             to={`${match.url}/detail/${book.id}`}>{book.title}</NavLink></li>)}
+                {books.map((book) =>
+                    <li key={book.id}>
+                        <NavLink activeClassName="activeV2" to={`${match.url}/detail/${book.id}`}>{book.title}</NavLink> |
+                        <a href="#" onClick={(event, id) => this.handleDeleteBook(event, id)}> delete </a> |
+                        <Link to={`${match.url}/edit/${book.id}`}>edit</Link>
+                    </li>
+                )}
             </ul>
             <Link to={`${match.url}/add`}>Add book</Link>
 
-            <div style={{ backgroundColor: "lightGray", padding: 5, marginTop: 10 }}>
-                <Route path={`${match.url}/add`} render={(props) => <AddBook bookStore={bookStore}
-                                                                             onAddBook={this.onBookWasAdded} />} />
+            <div style={{backgroundColor: "lightGray", padding: 5, marginTop: 10}}>
+                <Route path={`${match.url}/add`} render={(props) =>
+                    <AddBook buttonText="save" bookStore={bookStore} onBookStoreChange={this.onBookstoreWasChanged}/>
+                }/>
                 <Route path={`${match.url}/detail/:id`} render={(props) => {
-                    return (<Details {...props} bookStore={bookStore} />)
-                }} />
+                    return (<Details {...props} bookStore={bookStore}/>)
+                }}/>
+                <Route path={`${match.url}/edit/:id`} render={(props) =>
+                    <EditBook buttonText="edit" bookStore={bookStore} {...props} onBookStoreChange={this.onBookstoreWasChanged}/>
+                }/>
             </div>
         </div>)
     }
 }
+
 //Views end
 
 class Details extends React.Component {
     render() {
         let id = this.props.match.params.id;
-        let book = this.props.bookStore.books.filter((book) => {
-            return book.id === Number(id);
-        })[0];
+        let book = this.props.bookStore._books.find((book) => book.id === Number(id));
         return (
-            <div style={{ padding: 4 }}>
-                <h4 style={{ color: "steelblue" }}>Detailed info for the title: {book.title}</h4>
+            <div style={{padding: 4}}>
+                <h4 style={{color: "steelblue"}}>Detailed info for the title: {book.title}</h4>
                 <p>Info: {book.info}</p>
-                <br />
+                <br/>
                 <Link to="/products">Products</Link>
             </div>
         );
@@ -119,14 +165,16 @@ export default class App2 extends React.Component {
     constructor() {
         super();
     }
+
     render() {
         return (
-            <Router >
+            <Router>
                 <div>
-                    <Header />
+                    <Header/>
                     <Switch>
-                        <Route path="/products" render={(props) => (<Product {...props} bookStore={this.props.bookStore} />)} />
-                        <Route path="/company" component={Company} />
+                        <Route path="/products"
+                               render={(props) => (<Product {...props} bookStore={this.props.bookStore}/>)}/>
+                        <Route path="/company" component={Company}/>
                         <Route component={Home}></Route>
                     </Switch>
                 </div>
--- src/bookstore.js
+++ src/bookstore.js
@@ -2,21 +2,43 @@ class BookStore {
 
     constructor() {
         this._books = [
-            { id: 1,title: "How to Learn JavaScript - Vol 1", info: "Study hard"},
-            { id: 2,title: "How to Learn ES6", info: "Complete all exercises :-)"},
-            { id: 3,title: "How to Learn React",info: "Complete all your CA's"},
-            { id: 4,title: "Learn JavaScript, React and MobX",info: "Don't drink beers, until Friday (after four)"
-            }
+            {id: 1, title: "How to Learn JavaScript - Vol 1", info: "Study hard"},
+            {id: 2, title: "How to Learn ES6", info: "Complete all exercises :-)"},
+            {id: 3, title: "How to Learn React", info: "Complete all your CA's"},
+            {id: 4, title: "Learn JavaScript, React and MobX", info: "Don't drink beers, until Friday (after four)"}
         ]
-        this._nextID= 5;
+        this._nextID = 5;
     }
-    get books(){
+
+    get books() {
         return this._books;
     }
-    addBook(book){
-        book.id = this._nextID;
+
+    addBook(book) {
+        book.id = this._nextID++;
         this._books.push(book);
-        this._nextID++;
+    }
+
+    deleteBook(id) {
+        let targetedIndex = this.findIndexById(id)
+        if (targetedIndex) {
+            this._books.splice(targetedIndex, 1);
+        }
+    }
+
+    findIndexById(id) {
+        return this._books.findIndex((book) => book.id === id);
+    }
+
+    getBook(id) {
+        return this._books.find((book) => book.id === id)
+    }
+
+    editBook(book) {
+        let targetedIndex = this.findIndexById(book.id)
+        if (targetedIndex) {
+            this._books[targetedIndex] = book;
+        }
     }
 }
 
